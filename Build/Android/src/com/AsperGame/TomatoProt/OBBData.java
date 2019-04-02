7ecf0256f1095bb6f68339f603a7d1d606ab5389
Abdul <grawires@gmail.com>
2019-04-02 19:24:38 +0200

    a

--- Build/Android/src/com/AsperGame/TomatoProt/OBBData.java
+++ Build/Android/src/com/AsperGame/TomatoProt/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "12", // the version of the APK that the file was uploaded against
-203236904L // the length of the file in bytes
+203255089L // the length of the file in bytes
 )
 };
 };
